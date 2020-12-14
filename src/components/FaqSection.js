f823ac051ea0a7cb586f95d2632519dc6464dca4
git@github.com:curvesy <sinakorhanishirazi3494@gmail.com>
2020-12-14 07:10:37 -0700

    4.Faq Section

---
+++ src/components/FaqSection.js
@@ -0,0 +1,54 @@
+import React from 'react';
+
+const FaqSection = () => {
+  return (
+    <div className="faq">
+      <h2>
+        Any Questions <span>FAQ</span>
+      </h2>
+
+      <div className="question">
+        <h4>How Do I Start?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur,
+            adipisicing elit. Sed, possimus?
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Dialy Schedule</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur,
+            adipisicing elit. Sed, possimus?
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Deferrent Payment Method</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur,
+            adipisicing elit. Sed, possimus?
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>What Product do you offer?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur,
+            adipisicing elit. Sed, possimus?
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FaqSection;
