c53cfa024ab1af16d142d95c9b0127dc747e383b
git@github.com:curvesy <sinakorhanishirazi3494@gmail.com>
2020-12-14 14:42:59 -0700

    5.Intro io styled component

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,22 +1,23 @@
 import React from 'react';
 import home1 from '../img/home1.png';
+import styled from 'styled-components';
 
 const AbouteSection = () => {
   return (
-    <div>
-      <div className="description">
+    <About>
+      <Description>
         <div className="title">
-          <div className="hide">
+          <Hide>
             <h2>We Work to make</h2>
-          </div>
-          <div className="hide">
+          </Hide>
+          <Hide>
             <h2>
               your <span>dream</span> come
             </h2>
-          </div>
-          <div className="hide">
+          </Hide>
+          <Hide>
             <h2>true.</h2>
-          </div>
+          </Hide>
         </div>
         <p>
           Contact us for aWny photography or videography
@@ -24,12 +25,44 @@ const AbouteSection = () => {
           amazing skills
         </p>
         <button>Cantact Us</button>
-      </div>
-      <div className="image">
+      </Description>
+
+      <Image>
         <img src={home1} alt="guy with camera" />
-      </div>
-    </div>
+      </Image>
+    </About>
   );
 };
+
+const About = styled.div`
+  min-hight: 90vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 5rem 10rem;
+  color: white;
+`;
+
+const Description = styled.div`
+  flex: 1;
+  padding-right: 5rem;
+  h2 {
+    font-weight: lighter;
+  }
+`;
+
+const Image = styled.div`
+  flex: 1;
+  overflow: hidden;
+  img {
+    width: 100%;
+    hight: 80vh;
+    object-fit: cover;
+  }
+`;
+
+const Hide = styled.div`
+  overflow: hidden;
+`;
 
 export default AbouteSection;
